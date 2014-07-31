d149857b5ce7b2a132667583d78800039d1a18d9
BaiCanLin <A@MacBook.local>
2014-07-31 23:45:20 +0800

    Initial Commit

---
+++ 企信通/QXTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QXTAppDelegate.h
+//  企信通
+//
+//  Created by 林柏参 on 14/7/31.
+//  Copyright (c) 2014年 林柏参. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QXTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 企信通/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  企信通
+//
+//  Created by 林柏参 on 14/7/31.
+//  Copyright (c) 2014年 林柏参. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QXTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QXTAppDelegate class]));
+    }
+}
